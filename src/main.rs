eb5486fe7c11afce6a7b39fd92f87229d2e0dd03
Emanuel Skrenkovic <eskrenkovic@gmail.com>
2022-03-23 19:00:39 +0100

    cleand up REPL a tiny bit

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ pub mod scan;
 pub mod block;
 pub mod compiler;
 
-use std::io::{self, BufRead};
 use std::fs;
 use std::env;
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::io::{stdin, stdout, Write};
 
 use crate::vm::VM;
 use crate::block::Block;
@@ -17,18 +17,22 @@ fn main() {
     let mut args: Vec<String> = env::args().collect();
 
     if args.len() <= 1 {
+        let mut input = String::new();
+
         loop {
-            let stdin = io::stdin();
-            for line in stdin.lock().lines() {
+            print!("> ");
+            stdout().flush();
+            stdin().read_line(&mut input);
 
-                let block = Rc::new(RefCell::new(Block::new(256)));
+            let block = Rc::new(RefCell::new(Block::new(256)));
 
-                let mut compiler = Compiler::new(line.unwrap(), block.clone());
-                compiler.compile();
+            let mut compiler = Compiler::new(input.clone(), block.clone());
+            compiler.compile();
 
-                let mut vm = VM::new(block.clone());
-                vm.interpret()
-            }
+            let mut vm = VM::new(block.clone());
+            vm.interpret();
+
+            input.clear();
         }
     } else {
         let source = fs::read_to_string(&args[1]).unwrap();
