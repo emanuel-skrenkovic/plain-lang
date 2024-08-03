8c07a0553ddd7321e252a1df87a7f2746cbf346f
Emanuel Skrenković <eskrenkovic@gmail.com>
2024-08-03 09:31:13 +0200

    basic unary expression tests

--- plang/src/parse_test.rs
+++ plang/src/parse_test.rs
@@ -1,16 +1,102 @@
+use crate::ast;
+use crate::scan;
+use crate::error;
+use crate::parse::*;
+
+#[allow(dead_code)]
+fn parse_source(source: &str) -> Result<Vec<ast::Node>, Vec<error::Error>>
+{
+    let mut scanner = scan::Scanner::new(source.to_string());
+    let reporter    = error::Reporter::new(&source);
+    let tokens      = scanner.scan_tokens();
+
+    let parser = Parser::new(reporter, tokens);
+
+    // Act
+    parser.parse()
+}
+
+#[cfg(test)]
+mod unary_expression
+{
+    use super::*;
+    use crate::ast;
+    use crate::scan;
+
+    #[test]
+    fn test_minus()
+    {
+        let source = "-2";
+        let tree = parse_source(source);
+
+        // Assert
+        assert!(tree.is_ok());
+        let tree = tree.unwrap();
+
+        assert_eq!(tree.len(), 1);
+        let ast::Node::Stmt(ast::Stmt::Expr { expr }) = &tree[0] else {
+            panic!()
+        };
+
+        let ast::Expr::Unary { operator, expr } = &expr.value else {
+            panic!("Expect binary expression, found {:?}.", &expr.value)
+        };
+
+        assert_eq!(operator.kind.discriminant(), scan::TokenKind::Minus.discriminant());
+
+        let ast::Expr::Literal { value } = &expr.value else {
+            panic!("Expect literal, found {:?}", &expr.value);
+        };
+
+        assert_eq!(value.kind.discriminant(), scan::TokenKind::Literal.discriminant());
+
+    }
+
+    #[test]
+    fn test_not()
+    {
+        let source = "!false";
+        let tree = parse_source(source);
+
+        // Assert
+        assert!(tree.is_ok());
+        let tree = tree.unwrap();
+
+        assert_eq!(tree.len(), 1);
+        let ast::Node::Stmt(ast::Stmt::Expr { expr }) = &tree[0] else {
+            panic!()
+        };
+
+        let ast::Expr::Unary { operator, expr } = &expr.value else {
+            panic!("Expect binary expression, found {:?}.", &expr.value)
+        };
+
+        assert_eq!(operator.kind.discriminant(), scan::TokenKind::Bang.discriminant());
+
+        let ast::Expr::Literal { value } = &expr.value else {
+            panic!("Expect literal, found {:?}", &expr.value);
+        };
+
+        assert_eq!(value.kind.discriminant(), scan::TokenKind::False.discriminant());
+
+    }
+
+}
+
 #[cfg(test)]
 mod binary_expression 
 {
+    use super::*;
     use crate::ast;
     use crate::scan;
-    use crate::error;
-    use crate::parse::*;
 
     #[test]
     fn test_equals()
     {
         // Arrange
         let source = "2 == 3";
+
+        // Act
         let tree = parse_source(source);
 
         // Assert
@@ -601,15 +687,6 @@ mod binary_expression
         assert_eq!(right_value.kind.discriminant(), scan::TokenKind::Literal.discriminant());
     }
 
-    fn parse_source(source: &str) -> Result<Vec<ast::Node>, Vec<error::Error>>
-    {
-        let mut scanner = scan::Scanner::new(source.to_string());
-        let reporter    = error::Reporter::new(&source);
-        let tokens      = scanner.scan_tokens();
-
-        let parser = Parser::new(reporter, tokens);
-
-        // Act
-        parser.parse()
-    }
 }
+
+
